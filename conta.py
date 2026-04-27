298210215e45d6928327a7b484069729454cce42
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-04-27 11:46:45 -0300

    Add files via upload

---
+++ conta.py
@@ -0,0 +1,21 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+class Video:
+    def __init__(self, titulo, duracao, views):
+        self.titulo = titulo
+        self.duracao = duracao
+        self.views - views
+
+video = Video("Titanic", )
+class Livro:
+    def __init__(self, titulo, autor, data_publicacao):
+        self.titulo = titulo
+        self.autor = autor
+        self.data_publicacao = data_publicacao
+
+livro = Livro("Harry Potter", "J.K Roling", 1997-06-26)
---
+++ exec.py
@@ -0,0 +1,11 @@
+from conta import Conta
+
+conta = Conta(5420, "Matheus", 50.0, 300.00)
+print (conta)
+
+from exercicio import MConta
+
+conta1 = MConta (1, "Tadeu", 0.0)
+conta1 = MConta (2, "José", 0.0)
+conta1 = MConta (3, "Vanessa", 0.0, 2000.0)
+
